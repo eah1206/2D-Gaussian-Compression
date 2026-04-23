997a111a39f03c0914fc54c3165597ff105ce707
Ethan Hernandez <111538924+eah1206@users.noreply.github.com>
2026-04-23 13:34:22 -0400

    Added residual program

---
+++ residuals.py
@@ -0,0 +1,35 @@
+import cv2
+
+gif = cv2.VideoCapture('Freddy Jumpscare.gif')
+
+frames = []
+
+while True:
+    end, frame = gif.read()
+    
+    if not end:
+        break
+
+    # Add each frame of the gif to an array
+    frames.append(frame)
+
+    # Process 'frame' (it is already a NumPy array for OpenCV)
+    cv2.imshow('GIF Frame', frame)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+gif.release()
+cv2.destroyAllWindows()
+
+for i in range(1, len(frames)):
+    frame1 = frames[i-1]
+    frame2 = frames[i]
+
+    residual = cv2.absdiff(frame1, frame2)
+
+    cv2.imshow('Residual', residual)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
